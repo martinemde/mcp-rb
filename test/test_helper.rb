b13508f61bd8dd57bcb5ba162fa7ed36ade4502e
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-19 09:15:25 +0900

    Return initialize response in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,7 +24,7 @@ module MCPTest
         },
         id: 1
       }
-      server.send(:handle_request, init_request)
+      initialize_response = server.send(:handle_request, init_request)
 
       init_notification = {
         jsonrpc: MCP::Constants::JSON_RPC_VERSION,
@@ -32,6 +32,8 @@ module MCPTest
         id: 2
       }
       server.send(:handle_request, init_notification)
+
+      initialize_response
     end
   end
 end
