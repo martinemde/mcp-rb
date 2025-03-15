65f4d4f067ae6a36ad93ab0ba50f378c97295dbc
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-15 23:57:15 +0900

    Refactor test

--- test/snapshots_test.rb
+++ test/snapshots_test.rb
@@ -5,14 +5,8 @@ require_relative "test_helper"
 
 class SnapshotsTest < MCPTest::TestCase
   def test_hello_world_server_interaction
-    filename = "examples/hello_world.rb"
-    root_path = File.expand_path("..", __dir__)
-    server_path = File.join(root_path, filename)
-    snapshot = []
-    IO.popen(["ruby", server_path], "r+") do |server_io|
-      server_io.sync = true
-
-      messages = [
+    with_started_server("examples/hello_world.rb") do |server_io|
+      client_messages = [
         # Initialize request
         '{"jsonrpc": "2.0", "method": "initialize", "params": {"protocolVersion": "2024-11-05"}, "id": 1}',
         # Initialized notification
@@ -32,19 +26,35 @@ class SnapshotsTest < MCPTest::TestCase
         # Read nested resource template request
         '{"jsonrpc": "2.0", "method": "resources/read", "params": {"uri": "users://test/posts/3"}, "id": 9}'
       ]
-      messages.each do |msg|
-        snapshot << "Client: #{msg}\n"
-        server_io.puts msg
-        server_io.flush
-        server_io.wait_readable(0.5)
+      snapshot_text = record_interaction(server_io, client_messages)
 
-        while server_io.ready?
-          response = server_io.gets(chomp: true)
-          snapshot << "Server: #{response}\n"
-        end
+      assert_matches_snapshot snapshot_text
+    end
+  end
+
+  private
+
+  def with_started_server(filename)
+    root_path = File.expand_path("..", __dir__)
+    server_path = File.join(root_path, filename)
+    IO.popen(["ruby", server_path], "r+") do |server_io|
+      server_io.sync = true
+      yield server_io
+    end
+  end
+
+  def record_interaction(server_io, client_messages)
+    snapshot = []
+    client_messages.each do |msg|
+      snapshot << "Client: #{msg}\n"
+      server_io.puts msg
+      server_io.wait_readable(0.5)
+
+      while server_io.ready?
+        response = server_io.gets(chomp: true)
+        snapshot << "Server: #{response}\n"
       end
     end
-
-    assert_matches_snapshot snapshot.join
+    snapshot.join
   end
 end
