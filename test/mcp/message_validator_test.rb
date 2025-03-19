e6229513d83a7e58647441c8b575b7d259bf7911
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-20 01:23:59 +0900

    Improve validator unit tests

--- test/mcp/message_validator_test.rb
+++ test/mcp/message_validator_test.rb
@@ -7,7 +7,7 @@ module MCP
     def test_invalid_message
       validator = MessageValidator.new
 
-      invalid_message = {"not" => "a jsonrpc message"}
+      invalid_message = valid_mcp_message.except("jsonrpc")
 
       assert_raises(MessageValidator::InvalidMessage) do
         validator.validate_client_message!(invalid_message)
@@ -17,31 +17,32 @@ module MCP
     def test_unknown_method
       validator = MessageValidator.new
 
-      invalid_message = {
-        "jsonrpc" => "2.0",
-        "method" => "invalidMethod",
-        "params" => {},
-        "id" => 1
-      }
+      message_with_unknown_method = valid_mcp_message.merge("method" => "invalidMethod")
 
       assert_raises(MessageValidator::UnknownMethod) do
-        validator.validate_client_message!(invalid_message)
+        validator.validate_client_message!(message_with_unknown_method)
       end
     end
 
     def test_invalid_params
       validator = MessageValidator.new
 
-      invalid_message = {
-        "jsonrpc" => "2.0",
-        "method" => "resources/read",
-        "params" => {"invalid_param" => "value"},
-        "id" => 1
-      }
+      message_with_invalid_params = valid_mcp_message.merge("params" => {"invalid_param" => "value"})
 
       assert_raises(MessageValidator::InvalidParams) do
-        validator.validate_client_message!(invalid_message)
+        validator.validate_client_message!(message_with_invalid_params)
       end
+    end
+
+    private
+
+    def valid_mcp_message
+      {
+        "jsonrpc" => "2.0",
+        "method" => "resources/read",
+        "params" => {"uri" => "hello://world"},
+        "id" => 1
+      }
     end
   end
 end
